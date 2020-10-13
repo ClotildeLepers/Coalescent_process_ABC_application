7c7508e2aefe203799d9a6c383ae5c68cfb1af00
Chi Tran <42634078+tranvietchi15@users.noreply.github.com>
2020-10-13 19:24:49 +0200

    Update analysis_CentralAsiaHumanPop.R

--- analysis_CentralAsiaHumanPop.R
+++ analysis_CentralAsiaHumanPop.R
@@ -23,24 +23,24 @@ sum(is.na(donnees))
 
 
 # computation of the posterior probabilities of the alternatives H0 : b0=b1 and Ha : b0<b1
-priors=rep(0,201)
+posteriors=rep(0,201)
 
-priors=colSums(donnees[bool,14:214],na.rm=TRUE)
+posteriors=colSums(donnees[bool,14:214],na.rm=TRUE)
 abs=seq(1,200,1)
-plot(abs[1:100],priors[1:100],ylim=c(0,1),xlim=c(0,200),ylab="Pr(Ha | Sobs)",xlab="simulated sample")
-points(abs[101:200],priors[101:200],col='red')
+plot(abs[1:100],posteriors[1:100],ylim=c(0,1),xlim=c(0,200),ylab="Pr(Ha | Sobs)",xlab="simulated sample")
+points(abs[101:200],posteriors[101:200],col='red')
 lines(c(100,100),c(0,1),lty=3)
 
 legend(x="topright", legend=c("b0=b1", "b0<b1"), col=c("black", "red"),lwd=1,cex=0.8)
 
-mean(priors[1:100])
-median(priors[1:100])
-lines(c(0,100),c(median(priors[1:100]),median(priors[1:100])))
+mean(posteriors[1:100])
+median(posteriors[1:100])
+lines(c(0,100),c(median(posteriors[1:100]),median(posteriors[1:100])))
 text(x=45,y=0.05,"median prob = 0.4335",cex=0.75)
 
-mean(priors[101:200])
-median(priors[101:200])
-lines(c(100,200),c(median(priors[101:200]),median(priors[101:200])),col="red")
+mean(posteriors[101:200])
+median(posteriors[101:200])
+lines(c(100,200),c(median(posteriors[101:200]),median(posteriors[101:200])),col="red")
 text(x=155,y=0.05,"median prob = 0.5843",cex=0.75)
 
 # choice of the threshold alpha defining the critical region {P(H_a | Sobs)>alpha}.
@@ -52,8 +52,8 @@ type2=rep(0,101)
 
 for(i in 1:101)
 {
-  type1[i]=sum(priors[1:100]>alpha[i])/100
-  type2[i]=sum(priors[101:200]<=alpha[i])/100
+  type1[i]=sum(posteriors[1:100]>alpha[i])/100
+  type2[i]=sum(posteriors[101:200]<=alpha[i])/100
 }
 
 # Graphs of the two errors means (the a priori distribution gives probabilities 1/2,1/2 to the hypotheses "b0=b1" and "b0<b1")
@@ -62,12 +62,12 @@ alpha[which.min(error)]
 which.min(error)
 type1[which.min(error)]
 type2[which.min(error)]
-(median(priors[1:100])+median(priors[101:200]))/2
-median(priors[1:200])
+(median(posteriors[1:100])+median(posteriors[101:200]))/2
+median(posteriors[1:200])
 
 plot(alpha,error,ylim=c(0,1),xlim=c(0,1),ylab="P(Ha | Sobs, H0)+P(H0 | Sobs,Ha)",type='l')
 lines(c(alpha[which.min(error)],alpha[which.min(error)]),c(0,1),lty=2)
-lines(c((median(priors[1:100])+median(priors[101:200]))/2,(median(priors[1:100])+median(priors[101:200]))/2),c(0,1),lty=3)
+lines(c((median(posteriors[1:100])+median(posteriors[101:200]))/2,(median(posteriors[1:100])+median(posteriors[101:200]))/2),c(0,1),lty=3)
 
 legend(x="topright",legend=c("Sum of errors of types 1 and 2 minimal", "Mean between the medians under H0 and Ha"),col=c("black","black"),lty=c(2,3),cex=0.6)
 
@@ -93,11 +93,11 @@ alpha[54]
 
   # the column 201 corresponds to the weights computed for the summary statistics from Chaix et al. 2007 and Heyer et al. 2015 (see file data_central_Asia.csv)
 
-priors[201]
+posteriors[201]
   # 0.4517<0.5, so we do not reject the null hypothesis H0:b0=b1. 
   # Hence,the test concludes that there is no significant difference of fertility rates between the two social orga-nizations: patrilineal and cognatic.
 
-sum(priors[1:100]>priors[201])/100
+sum(posteriors[1:100]>posteriors[201])/100
 
 summary(donnees$b0)
 summary(donnees$b1)
@@ -152,7 +152,7 @@ for(i in 1:100)
   ecartsb[i]=donnees$b1[booli]-donnees$b0[booli]
 }
 o=order(ecartsb)
-priorsdiff=priors[101:200]
-pourcentagetype2=cumsum(priorsdiff[o]<=0.29)/100
+posteriorsdiff=posteriors[101:200]
+pourcentagetype2=cumsum(posteriorsdiff[o]<=0.29)/100
 plot(ecartsb[o],pourcentagetype2, type="l",ylim=c(0,1),xlab="b1-b0",ylab="Cum. freq. of Type II err.")
 lines(density(ecartsb),col="red")
